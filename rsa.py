5e9f40abb47410fd832abffdf9ce25cf98825119
CAS <cas@codepad.fritz.box>
2022-12-24 13:12:55 +0100

    new coprime methods

--- rsa.py
+++ rsa.py
@@ -8,6 +8,7 @@
 ╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝
 """
 import math
+import random
 from random import getrandbits
 
 MESSAGE = 'hello'
@@ -31,34 +32,58 @@ def is_prime(n: int) -> bool:
             return False
     return True
 
-# create prime candidate & check if prime
-def make_prime(l: int) -> int:
+# create prime candidate & check if prime of length l bits
+def make_primes(l: int) -> int:
     p = rand_bits(l)
+    q = rand_bits(l)
     while is_prime(p) == False:
         p = rand_bits(l)
-    return p
+    while is_prime(q) == False and p != q:
+        q = rand_bits(l)
+    return p, q
 
 # return number between 1 & m that is coprime with both m & n
-def find_coprime(m: int, n: int) -> int:
-    for i in range(2, m):
-        # greatest common divisor (ggT)
-        if math.gcd(i, m) == 1 & math.gcd(i, n) == 1:
-            return i
-    return 1
+def random_coprime(m: int) -> int:
+    a = random.randint(2, m - 1)
+    while math.gcd(a, m) != 1:
+        a = random.randint(2, m - 1)
+    return a
 
 # compute keys
-def make_keys() -> dict:
-    p = 5#make_prime(BITS)
-    q = 11#make_prime(BITS)
+def generate_keys() -> dict:
+    """
+    p & q -> prime
+    p != q
+    """
+    p, q = make_primes(BITS)
+    # p = 2
+    # q = 7 
     
     n = p * q
+    # len/amount of non-common factors between 1 and n
     m = (p-1) * (q-1)
 
-    a = find_coprime(m, n)
-    return {
-        'private': m,
-        'public': (a, n)
-    }
+    # find the ENCRYPTION key
+    """ requirements:
+    random int between 1 and m (exclusive)
+    coprime to m
+    """
+    a = random_coprime(m)
 
-keys = make_keys()
+    # find the DECRYPTION key
+    """ requirements:
+    a * d % m = 1
+    """
+    # self-recursion until d != a
+    d = pow(a, -1, m)
+    if d == a:
+        return generate_keys()
+    # requirement met first try
+    else:
+        return {
+            'encryption': (a, n, d),
+            'private': m
+        }
+
+keys = generate_keys()
 print(keys)
